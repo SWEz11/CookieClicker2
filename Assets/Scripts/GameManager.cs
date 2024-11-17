e389ccbc19e7c4ae1d77a4e404a6ed70c34225f0
SWEz11 <137026304+SWEz11@users.noreply.github.com>
2024-11-17 11:19:21 +0200

    ok

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,13 @@ public class GameManager : MonoBehaviour
     public int score;
     public TMP_Text scoreText;
     public int clicksperclick;
+    public int[] clickgoal;
+    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer particalRenderes;
+    public Sprite[] nextcookie;
+    public Sprite[] nextpartical;
+    int skinIndex;
+    public GameObject unlockpartical;
 
     void Start()
     {
@@ -23,6 +30,13 @@ public class GameManager : MonoBehaviour
             transform.localScale -= Vector3.one * 0.1f;
         }
         SaveScore();
+
+        if(skinIndex < clickgoal.Length && score >= clickgoal[skinIndex])
+        {
+            unlockpartical.SetActive(true);
+            spriteRenderer.sprite = nextcookie[skinIndex++];
+            particalRenderes.sprite = nextpartical[skinIndex++];
+        }
     }
 
     void OnMouseDown()
